d5786b4a58faf2e4bd6fdcbad42c0986105eba43
Matthew <matthewkimcs@gmail.com>
2023-01-14 21:58:47 -0800

    added modal functionality

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
 "use strict";
 
 const mainContainer = document.querySelector(".main");
+const addBookBtn = document.querySelector(".add-book");
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+
+function closeModal(e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+  }
+}
+
+function openModal() {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+}
+
+addBookBtn.addEventListener("click", openModal);
+
+document.addEventListener("keydown", closeModal);
