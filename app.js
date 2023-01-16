afeaf7a55ecbd1c99932ff0f3d9ccf5010a16486
Matthew <matthewkimcs@gmail.com>
2023-01-16 11:09:34 -0800

    added the functionality of books being displayed on the webpage when a user submits a form

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const addBookBtn = document.querySelector(".add-book");
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const submitForm = document.querySelector(".submit-form");
+const title = document.getElementById("title");
+const author = document.getElementById("author_name");
+const pages = document.getElementById("pages");
 const myLibrary = [];
 
 function Book(title, author, pages) {
@@ -13,10 +16,14 @@ function Book(title, author, pages) {
   this.pages = pages;
 }
 
-function addBookToLibrary() {}
+function addBookToLibrary(event) {
+  event.preventDefault();
+  myLibrary.push(new Book(title.value, author.value, pages.value));
+  displayHTML(title.value, author.value, pages.value);
+}
 
-function closeModal(e) {
-  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+function closeModal(event) {
+  if (event.key === "Escape" && !modal.classList.contains("hidden")) {
     modal.classList.add("hidden");
     overlay.classList.add("hidden");
   }
@@ -27,8 +34,22 @@ function openModal() {
   overlay.classList.remove("hidden");
 }
 
+function displayHTML(title, author, pages) {
+  const html = `
+  <div class="book">
+    <p>${title}</p>
+    <p>${author}</p>
+    <p>${pages}</p>
+    <div class="button-group">
+      <button class="btn remove">Remove</button>
+    </div>
+  </div>
+  `;
+  mainContainer.insertAdjacentHTML("beforeend", html);
+}
+
 addBookBtn.addEventListener("click", openModal);
 
 document.addEventListener("keydown", closeModal);
 
-
+submitForm.addEventListener("click", addBookToLibrary);
